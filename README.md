25ebe4b0155550c73a42c102f9b839e1731193e2
becks9908 <becks9908@gmail.com>
2023-03-02 15:57:38 +0800

    new edit pm 03:57

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 # NXP_FPGROWTH
-
+## Description
+Use fpgrowth to quickly find similar fail testing files and improve the traditional manual query method.
 ## Current directory
 Description for each file in the directory:
-- [fp_growth.py]() - Main file.
-- [preprocess.py]() - Reading precess file.
-- [requirements.txt]() - Required dependencies that need to be installed.
-- [Datafile_0222]() - Folder of simulation files, a total of 100 records.
-- [reference_result]() - Folder of the generated file example.
+- [fp_growth.py](https://github.com/becks9908/NXP_fpgrowt/blob/main/fp_growth.py) - Main file.
+- [preprocess.py](https://github.com/becks9908/NXP_fpgrowt/blob/main/preprocess.py) - Reading precess file.
+- [requirements.txt](https://github.com/becks9908/NXP_fpgrowt/blob/main/requirements.txt) - Required dependencies that need to be installed.
+- [baseline.py]() - Baseline file, simulate the way of manual search.
+- [Datafile_0222](https://github.com/becks9908/NXP_fpgrowt/tree/main/Datafile_0222) - Folder of simulation files, a total of 100 records.
+- [reference_result](https://github.com/becks9908/NXP_fpgrowt/tree/main/reference_result) - Folder of the generated file example.
 - [Result]() - If the folder does not exist initially, it will generate later.
 
 ## Before execution
+
+**Installing :**
+```ssh
+pip install -r requirements.txt
+```
+
 **To edit the file for this project, follow these steps:**
 1. Open the "fp_growth.py" file in the directory.
 2. Make your changes to the file.
@@ -40,14 +48,47 @@ SHEET_NAME = 'Data_1' # Excel sheet name
 ```
 
 ## Start execution
+**Run :**
 ```sh
 python fp_growth.py
 ```
 **If "Result" folder does not exist initially, it will generate :**
 ##### Result:
-- [rankingResult.json]() - The total ranking of test files for each training file.
-- [train_fail_rate_result_file.json]() - Store the name of the fail testname in each training file.
-- [test_fail_rate_result_file.json]() - Store the name of the fail testname in each test file.
+- [train_fail_rate_result_file.json]() - Store the name of the fail testname in each training file. 
+(**Notice : If you need to regenerate the training results, please delete train_fail_rate_result_file.json in this folder.)**
 - [test_file_score_dic.json]() - Store K rule scores after sorting each test file pair.
+- [rankingResult.json]() - The total ranking of test files for each training file.
+- [Baseline_similars_result.json]() - Results after Baseline simulation.
 
-# NXP_fpgrowt
+
+## The baseline
+**If you want to compare the baseline time, please follow :**
+1. Open the "baseline.py" file in the directory.
+2. Make your changes to the file.
+3. Save the file and exit.
+
+**Adjust color parameters :**
+ 
+```python
+# Font color setting, default red
+FAIL = "00FF0000"
+```
+
+
+**Adjust file setting :**
+```python
+# file read setting
+TRAIN = 10            # Number of training files
+TEST = 10             # Number of testing files
+DATA_PATH = ''        # Put the path of dataset file dictionary
+SHEET_NAME = 'Data_1' # Excel sheet name
+
+START_ROW = 24      # adjust Started testcase column
+TESTNUM_COLUMN = 2  # adjust Started testnum column
+TESTNAME_COLUMN = 3 # adjust Started testname column
+```
+
+**Run :**
+```sh
+python baseline.py
+```
---
+++ baseline.py
@@ -0,0 +1,121 @@
+import os
+import string
+import openpyxl
+from openpyxl.styles import Font, colors
+from random import sample
+import time
+import numpy
+import json
+import glob
+import natsort
+
+# Font color setting
+FAIL = "00FF0000"
+
+# file read setting
+TRAIN = 10
+TEST = 10
+DATA_PATH = '/Users/beckswaz/Desktop/NXP_fpgrowth/Datafile_0222/'
+SHEET_NAME = 'Data_1'
+
+START_ROW = 24
+TESTNUM_COLUMN = 2
+TESTNAME_COLUMN = 3
+
+# fail rate setting
+DATA_FILE_NAME = []
+DATA_FILE_LIST = []
+testFail = {}
+trainFail = {}
+Similarity = [{} for i in range(TEST)]
+
+
+def fail_calculate_TRAIN(DATA_FILE_LIST):
+    # read testfile
+    for file_path in DATA_FILE_LIST:
+        DATA_FILE_NAME.append(os.path.basename(file_path))
+    print("\n##############  1. Estimate Training File Fail Rate Process ##############\n")
+    # In Train case calculate which TestName is fail
+    for a in range(TRAIN):
+        wb = openpyxl.load_workbook(DATA_FILE_LIST[a])
+        print("========== Estimating training file " +
+              DATA_FILE_NAME[a] + " ==========")
+        s2 = wb[SHEET_NAME]
+        # save the list of testing testname
+        targetList = []
+        for i in range(START_ROW, s2.max_row+1):
+            cell_obj = s2.cell(row=i, column=TESTNUM_COLUMN)
+            if cell_obj.font.color is not None and cell_obj.font.color.rgb == FAIL:
+                targetList.append(s2.cell(row=i, column=TESTNAME_COLUMN).value)
+        trainFail[DATA_FILE_NAME[a]] = targetList
+    return trainFail
+
+
+def fail_calculate_TEST(DATA_FILE_LIST):
+    # read testfile
+    for file_path in DATA_FILE_LIST:
+        DATA_FILE_NAME.append(os.path.basename(file_path))
+    print("\n##############  2. Estimate testing file fail rate process ##############")
+    # In Test case calculate which TestName is fail
+    for b in range(TEST):
+        wb = openpyxl.load_workbook(DATA_FILE_LIST[b+TRAIN])
+        s2 = wb[SHEET_NAME]
+        print("========== Estimating training file " +
+              DATA_FILE_NAME[b+TRAIN] + " ==========")
+        # 把target的testname存進去list
+        targetList = []
+        for i in range(START_ROW, s2.max_row+1):
+            cell_obj = s2.cell(row=i, column=TESTNUM_COLUMN)
+            if cell_obj.font.color is not None and cell_obj.font.color.rgb == FAIL:
+                targetList.append(s2.cell(row=i, column=TESTNAME_COLUMN).value)
+        testFail[DATA_FILE_NAME[b+TRAIN]] = targetList
+    return testFail
+
+
+def similar(trainFail, testFail, DATA_FILE_NAME):
+    Rate = {}
+    similar = []
+
+    print("\n##############  3. Estimate the similarity between the Training/Testing file #####################")
+
+    for i in range(TEST):
+        rateDict = {}
+        for j in range(TRAIN):
+            cal_rate = 0
+            count = 0
+            for k in range(len(testFail.get(DATA_FILE_NAME[i+TRAIN]))):
+                if testFail.get(DATA_FILE_NAME[i+TRAIN])[k] in (trainFail.get(DATA_FILE_NAME[j])):
+                    count += 1
+                else:
+                    pass
+            cal_rate = count / len(numpy.unique((testFail.get(DATA_FILE_NAME[i+TRAIN]) +
+                                                (trainFail.get(DATA_FILE_NAME[j])))))
+            rateDict[DATA_FILE_NAME[j]] = cal_rate
+        similar = sorted(rateDict.items(),
+                         key=lambda item: item[1], reverse=True)
+        Rate[DATA_FILE_NAME[i]] = similar
+    print("\n##############  Estimation of similarity finished  #####################")
+    jsonFile = open("Result/Baseline_similars_result.json", "w")
+    jsonFile.write(json.dumps(Rate, indent=2))
+    jsonFile.close()
+
+
+def main():
+    # time calculate
+    start_time = time.time()
+
+    path = 'Result'
+    if not os.path.isdir(path):
+        os.makedirs(path)
+
+    #================== Sort the file names first ==================#
+    DATA_FILE_LIST = glob.glob(os.path.join(DATA_PATH, "*.xlsx"))
+    DATA_FILE_LIST = natsort.natsorted(DATA_FILE_LIST)
+    Training_Result = fail_calculate_TRAIN(DATA_FILE_LIST)
+    Test_Result = fail_calculate_TEST(DATA_FILE_LIST)
+    similar(Training_Result, Test_Result, DATA_FILE_NAME)
+    print("*********************************************\n*                                           *\n*         %s mins        *\n*                                           *\n*********************************************" % ((time.time() - start_time) / 60))
+
+
+if __name__ == "__main__":
+    main()
